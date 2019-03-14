b0b41d4b7a21b9e306af3c3800973a2a9b23079a
Cyprian Munene <munenecyp@gmail.com>
2019-03-14 11:01:55 +0300

    test the models

--- insta/tests.py
+++ insta/tests.py
@@ -33,16 +33,6 @@ class ImageTest(TestCase):
         photos = Image.objects.all().update()
         self.assertTrue(len(photos) > 0)
 
-    def test_get_image_by_id(self, id):
-        this_image = Image.objects.filter(id)
-        self.assertTrue(len(this_image) > 0)
-
-    def test_search_image(self):
-        self.img.save_photo()
-
-    def test_filter_by_location(self):
-        self.img.save_photo()
-
     def tearDown(self):
         Profile.objects.all().delete()
         Image.objects.all().delete()
