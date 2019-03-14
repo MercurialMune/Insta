a7fa5cd36d78d6d608b856abb895e5509eb24b27
Cyprian Munene <munenecyp@gmail.com>
2019-03-14 11:16:46 +0300

    update readme

--- insta/forms.py
+++ insta/forms.py
@@ -13,7 +13,14 @@ class ProfileEditForm(forms.ModelForm):
    class Meta:
       model = Profile
       fields = ['pic','bio']
+      widgets = {
+         'bio': forms.Textarea(attrs={'placeholder': 'Enter the bio..'})
+      }
 
 
-
-
+class UserEditForm(forms.ModelForm):
+   first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+   last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+   class Meta:
+      model = User
+      fields = ['first_name','last_name']
